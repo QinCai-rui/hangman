151ba97570bd342d0260225a1ef18c4e1f9972ea
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-07-19 12:02:59 +1200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,10 +1,26 @@
-# version 0.0.1
+# version 0.1.0
 
 import random
 
-words = ["python", "hangman", "challenge", "programming"]
-
 def get_word():
+    words = ['Apple', 'Bread', 'Chair', 'Dance', 'Eagle', 'Fruit', 'Grape', 'House', 'Ivory', 'Jelly', 
+    'Knife', 'Lemon', 'Mango', 'Night', 'Ocean', 'Peach', 'Queen', 'River', 'Stone', 'Tiger', 
+    'Union', 'Vivid', 'Whale', 'Xenon', 'Yield', 'Zebra', 'Blaze', 'Crane', 'Dream', 'Flame', 
+    'Globe', 'Haven', 'Jolly', 'Kneel', 'Lunar', 'Mirth', 'Noble', 'Orbit', 'Piano', 'Quilt', 
+    'Raven', 'Shine', 'Thorn', 'Unity', 'Valor', 'Waltz', 'Xylog', 'Yacht', 'Zealot', 'Animal',
+    'Banana', 'Castle', 'Dragon', 'Energy', 'Flower', 'Garden', 'Heaven', 'Island', 'Jungle',
+    'Kitten', 'Laptop', 'Monkey', 'Nature', 'Orange', 'Planet', 'Quiver', 'Rocket', 
+    'Sunset', 'Tunnel', 'Umbrella', 'Victory', 'Whisper', 'Xylophone', 'Yellow', 'Zephyr', 
+    'Beacon', 'Canyon', 'Desert', 'Empire', 'Forest', 'Galaxy', 'Harbor', 'Insect', 'Jigsaw', 
+    'Knight', 'Lantern', 'Mystic', 'Nectar', 'Oracle', 'Pirate', 'Quasar', 'Rescue', 'Savage', 
+    'Trophy', 'Utopia', 'Voyage', 'Wander', 'Xenial', 'Yonder', 'Zephyr', 'Adventure', 'Butterfly',
+    'Chocolate', 'Dinosaur', 'Elephant', 'Fireworks', 'Grapefruit', 'Happiness', 'Important', 'Jasmine',
+    'Kangaroo', 'Lighthouse', 'Mountain', 'Notebook', 'Orchestra', 'Pineapple', 'Question', 'Rainbow',
+    'Sunshine', 'Universe', 'Vacation', 'Wonderful', 'Xenophobia', 'Youthful', 'Zoologist', 'Beautiful',
+    'Champion', 'Delicious', 'Enchanted', 'Fantastic', 'Glamorous', 'Harmonize', 'Incredible', 'Joyfulness',
+    'Knowledge', 'Legendary', 'Magnificent', 'Nostalgia', 'Optimistic', 'Phenomenal', 'Quintuple', 'Radiation', 
+    'Spectacle', 'Triumphant', 'Unstoppable', 'Vibrantly', 'Whimsical', 'Xenophile', 'Yearning', 
+    'Zealously']
     return random.choice(words)
 
 def display_word(word, guessed_letters):
@@ -88,13 +104,14 @@ def play_hangman():
         guess = get_guess()
 
         if guess in guessed_letters:
-            print("You already guessed that letter.")
+            print("You already guessed that letter. Try a different one. ")
         elif guess in word_to_guess:
             guessed_letters.add(guess)
             if set(word_to_guess) <= guessed_letters:
                 print(f"Congratulations! You guessed the word: {word_to_guess}")
                 break
         else:
+            guessed_letters.add(guess)
             tries += 1
             display_hangman(tries)
 
